a492c114412e4e3a0302b4ac3730b4409270a7d0
Jose Rosas <josejrrosas@yahoo.com>
2023-06-14 20:00:44 -0500

    modified authUser to be async

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,12 @@
+import asyncHandler from 'express-async-handler'
 //@desc     Auth user/set token
 //route     POST /api/use/auth
 //@access   Public
-const authUser = (req, res) => {
+
+//asyncHandler allows us to use async await and not have to wrap 
+//everything in try catches
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
-};
+});
 
 export { authUser };
