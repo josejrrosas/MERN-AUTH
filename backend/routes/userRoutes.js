b3f011ebce3120fd043db4200125030a6f9bc20a
Jose Rosas <josejrrosas@yahoo.com>
2023-06-14 22:25:58 -0500

    routes connected to controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express'
-import { authUser } from '../controllers/userController.js';
+import { authUser,registerUser,logoutUser,getUserProfile,updateUserProfile } from '../controllers/userController.js';
 
 //create new router object
 const router = express.Router();
 
 //when an HTTP POST request is made to the '/auth' path, 
 //the authUser function will be called to handle the request. 
-router.post('/auth', authUser)
+router.post('/auth', authUser);
+router.post('/', registerUser);
+router.post('/logout', logoutUser);
+router.route('/profile').get(getUserProfile)/put(updateUserProfile);
+// router.get('/profile', getUserProfile)
+// router.put('/profile', updateUserProfile)
 
 export default router;
