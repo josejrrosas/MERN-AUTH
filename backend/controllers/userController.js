f6630bd1be312a87dc6732319d5e0b6460017d09
Jose Rosas <josejrrosas@yahoo.com>
2023-06-19 09:41:17 -0500

    logoutUser function destroys cookie now,added matchPassword funciton to userModel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,12 +5,30 @@ import generateToken from '../utils/generateToken.js'
 //asyncHandler allows us to use async await and not have to wrap
 //everything in try catches
 //-----------------------------------------------------------------
-//@desc     Auth user/set token
+//@desc     login/Auth user/set token
 //route     POST /api/users/auth
 //@access   Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+  const {email, password} = req.body;
+
+  const user = await User.findOne({email})
+
+  //calls matchPassword from userModel
+  //this if statement allows to check for both email and password 
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    //dont want to expose which is wrong to prevent validation for bad actor
+    throw new Error("Invalid email or password");
+  }
 });
+
 //-----------------------------------------------------------------
 //@desc     Register a new user
 //route     POST /api/users
@@ -51,7 +69,13 @@ const registerUser = asyncHandler(async (req, res) => {
 //route     POST /api/users/logout
 //@access   Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User" });
+//destroys cookie which in turn logs out user
+res.cookie('jwt', '', {
+  httpOnly: true,
+  expires: new Date(0),
+});
+
+  res.status(200).json({ message: "User logged out" });
 });
 //-----------------------------------------------------------------
 //@desc     Get User profile
