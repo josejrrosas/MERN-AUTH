25c385dfa1aa0e2715a83b77ebce4c9234cf0c45
Jose Rosas <josejrrosas@yahoo.com>
2023-06-20 13:36:19 -0500

    created hero component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header"
+import Homescreen from "./screens/Homescreen";
+
 const App = () => {
   return (
-    <h1>
-      MERN Auth
-    </h1>
-  )
-}
+    <>
+    <Header />
+    <Homescreen />
+    </>
+  );
+};
 
-export default App
+export default App;
