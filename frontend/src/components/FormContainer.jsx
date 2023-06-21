c11265124bf77aa89475a057d41c41e7ab487a33
Jose Rosas <josejrrosas@yahoo.com>
2023-06-21 12:27:54 -0500

    created FormContainer and LoginScreen

---
+++ frontend/src/components/FormContainer.jsx
@@ -0,0 +1,19 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+/*gonna wrap login and register form etc..,
+so take in prop of children bc whatever we wrap
+it in we want it to display in here*/
+// eslint-disable-next-line react/prop-types
+const FormContainer = ({ children }) => {
+  return (
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col xs={12} md={6} className="card p-5">
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default FormContainer;
